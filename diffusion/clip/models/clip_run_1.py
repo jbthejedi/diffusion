5fdaa5a05039246cee8f933abd67f724a09f6fee
Justin Barry <justinbarrythejedi@gmail.com>
2025-06-24 20:27:53 +0700

    change trans name to trranformers

--- diffusion/clip/models/clip_run_1.py
+++ diffusion/clip/models/clip_run_1.py
@@ -2,7 +2,7 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import diffusion.transform.transformer_modules as tm
+import diffusion.transformers.transformer_modules as tm
 
 
 class VisionTransformer(nn.Module):
